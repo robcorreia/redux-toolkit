0ad1bf443a09e11322b9c78a5e0c800acc74163f
Robson <robccorreia@gmail.com>
2022-08-29 19:38:20 -0300

    feat: add project 1 - counter

---
+++ project-1/src/App.js
@@ -0,0 +1,10 @@
+import { Counter } from "./features/counter/Counter";
+
+import "./App.css";
+export function App() {
+  return (
+    <div className="container">
+      <Counter />
+    </div>
+  );
+}
